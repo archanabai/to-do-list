41f8259ac3968450cdac9686870e40de790ca169
archanabai <121252186+archanabai@users.noreply.github.com>
2023-12-18 16:07:18 +0530

    update todo list

--- script.js
+++ script.js
@@ -13,14 +13,28 @@ function addTask(){
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         inputBox.value='';
-        
         let span = document.createElement("span");
-        // span.innerHTML="edit";
          li.appendChild(span);
-         span.addEventListener("click",()=>{
-            
-         })
-         
+        saveData();
     }
 }
+ listContainer.addEventListener("click" ,(e)=>{
+   if(e.target.tagName ==="LI"){
+    e.target.classList.toggle("checked");
+     saveData();
+
+   }else if(e.target.tagName === "SPAN"){
+    e.target.parentElement.remove();
+    saveData();
+   }
+ },false);
+
+ function saveData(){
+    localStorage.setItem("data" , listContainer.innerHTML)
+ }
+
+ function showData(){
+  listContainer.innerHTML = localStorage.getItem("data");
+ }
+ showData();
  
